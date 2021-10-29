4a98bea4e58ee749a4168f3103d6415a3e2bd196
김재성 <aamoos@naver.com>
2021-10-29 17:32:42 +0900

    친구 검색, 친구 삭제, 회원 탈퇴 기능 추가

--- src/main/java/com/chatting/repository/FriendsRepository.java
+++ src/main/java/com/chatting/repository/FriendsRepository.java
@@ -26,7 +26,30 @@ public interface FriendsRepository extends CrudRepository<Friends, Long> {
                     "T1.USER_ID = :userId\n"
             , nativeQuery = true
     )
-    List<Friends> findByUserId(String userId);
+    List<Friends> selectReciveNotification(String userId);
+
+    //친구검색
+    //받은 이력 조회
+    @Query(value =
+            "SELECT \n"+
+                    "T1.FRIENDS_IDX\n"+
+                    ",T1.USER_ID\n"+
+                    ",(SELECT T2.NICK_NAME FROM USERS T2 WHERE T2.USER_ID = T1.FRIENDS_ID) AS FRIENDS_NAME\n"+
+                    ",(SELECT T2.PROFILE_IDX FROM USERS T2 WHERE T2.USER_ID = T1.FRIENDS_ID) AS FRIENDS_PROFILE_IDX\n"+
+                    ",T1.FRIENDS_ID\n"+
+                    ",T1.REG_DATE\n"+
+                    "FROM \n"+
+                    "friends T1\n"+
+                    "WHERE\n"+
+                    "T1.USER_ID = :userId\n"+
+                    "AND (SELECT T2.NICK_NAME FROM USERS T2 WHERE T2.USER_ID = T1.FRIENDS_ID) LIKE CONCAT('%',:friendsName,'%')\n"
+            , nativeQuery = true
+    )
+    List<Friends> selectFriendsList(String friendsName, String userId);
 
     Friends findByFriendsIdAndUserId(String friendsId, String userId);
+
+    Friends findByFriendsIdx(Long friendsIdx);
+
+    List<Friends> findAllByFriendsIdOrUserId(String friendsId, String userId);
 }
